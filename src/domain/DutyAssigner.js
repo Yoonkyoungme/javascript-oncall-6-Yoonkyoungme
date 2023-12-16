d0f68c03726b3150becba6816e64852a6aeb8bbb
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2023-12-16 17:10:22 +0900

    feat: 시작 날부터 마지막 날까지 평일인지 판별하는 기능 구현

--- src/domain/DutyAssigner.js
+++ src/domain/DutyAssigner.js
@@ -7,22 +7,44 @@ class DutyAssigner {
 
   #holidayEmployees;
 
+  #monthlyScheduler;
+
+  #employees = [];
+
+  #employeesIndex = [0, 0];
+
   constructor(dutyScheduler, weekdayEmployees, holidayEmployees) {
     this.#dutyScheduler = dutyScheduler;
-    this.weekdayEmployees = weekdayEmployees;
-    this.holidayEmployees = holidayEmployees;
+    this.#weekdayEmployees = weekdayEmployees
+      .split(',')
+      .map((employee) => employee.trim());
+    this.#holidayEmployees = holidayEmployees
+      .split(',')
+      .map((employee) => employee.trim());
     this.assignDuty();
   }
 
   assignDuty() {
+    this.getMonthlyScheduler();
+    console.log(this.#employees);
+  }
+
+  getMonthlyScheduler() {
     const startDay = this.getStartDay();
     const endDay = this.getEndDay();
+    for (let start = startDay; start <= endDay; start += 1) {
+      const isWeekday = this.isWeekday(start);
+    }
+  }
+
+  getDay(date) {
+    const [month, startDay] = this.#dutyScheduler;
+    return new Date(2023, +month - 1, date).getDay();
   }
 
   getStartDay() {
-    const [month, startDay] = this.#dutyScheduler;
-    const firstDay = new Date(2023, +month - 1, 1).getDay();
-    const startDayIndex = DAY_OF_WEEK.indexOf(startDay) + 1;
+    const firstDay = this.getDay(1);
+    const startDayIndex = DAY_OF_WEEK.indexOf(this.#dutyScheduler[1]) + 1;
     const startDayDiff = startDayIndex - firstDay;
     return startDayDiff < 0 ? startDayDiff + 7 : startDayDiff;
   }
@@ -30,6 +52,14 @@ class DutyAssigner {
   getEndDay() {
     const [month, startDay] = this.#dutyScheduler;
     return new Date(2023, month, 0).getDate();
+  }
+
+  // 시작 요일부터 평일인지 판단하는 기능 (휴일: 토요일, 일요일, 법적공휴일 LEGAL_HOLIDAYS)
+  isWeekday(date) {
+    const day = new Date(2023, +this.#dutyScheduler[0] - 1, date).getDay();
+    const isWeekend = day === 0 || day === 6;
+    const isLegalHoliday = LEGAL_HOLIDAYS.includes(`5월 ${date}일`);
+    return !isWeekend && !isLegalHoliday;
   }
 }
 
